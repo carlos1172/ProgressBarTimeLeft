22f0cf8c4a078bb770dbf6f4ed3633ecf51973d3
carlos1172 <51019601+carlos1172@users.noreply.github.com>
2021-11-26 09:22:19 +0800

    Update reviewer_progress_bar.py

--- reviewer_progress_bar.py
+++ reviewer_progress_bar.py
@@ -31,19 +31,6 @@ from aqt.utils import tooltip
 
 __version__ = '2.0.1'
 
-#-------------Configuration------------------
-config = mw.addonManager.getConfig(__name__)
-# The default steps for "New" Anki cards are 1min and 10min meaning that you see New cards actually a minimum of *TWO* times that day
-# You can now configure how many times new cards will be counted.
-# CountTimesNew = 1 (old version)
-# Quantify '1' time the "new card" time | Example: Steps (10 1440)
-# CountTimesNew = 2 (default)
-# Quantify '2' times the "new card" time | Example: Steps (1 10)
-# CountTimesNew = n
-# Quantify 'n' times the "new card" time | Example: Steps (1 10 10 20 30...)
-CountTimesNew = config['CountTimesNew']
-#-------------Configuration------------------
-
 ############## USER CONFIGURATION START ##############
 
 # CARD TALLY CALCULATION
@@ -252,9 +239,9 @@ def updatePB() -> None:
         lrn += tree[3]
         due += tree[2]
 
-    #if CountTimesNew == 0: CountTimesNew = 2
-    total = (CountTimesNew*new) + lrn + due
-    totalDisplay = (CountTimesNew*new) + lrn + due
+    #if newWeight == 0: newWeight = 2
+    total = (newWeight*new) + lrn + due
+    totalDisplay = (newWeight*new) + lrn + due
     #total = new + lrn + due
 
     # Get studdied cards
